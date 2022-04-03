cd4bff5a3324b9efd877f17660bde90f6ab9190e
Ju4nPx <juanpmachadob@gmail.com>
2022-04-02 22:34:05 -0500

    Contact section with mailto:

--- client/src/components/Contact.vue
+++ client/src/components/Contact.vue
@@ -14,23 +14,12 @@
           </p>
         </div>
         <form class="form" @submit.prevent="checkForm()">
-          <div class="form-50">
-            <div class="form-item">
-              <label for="name" class="required">{{ $t("fields.name") }}</label>
-              <input type="text" name="name" id="name" v-model="name" />
-              <small class="form-invalid-msg" v-if="errors['name']">{{
-                errors["name"][0]
-              }}</small>
-            </div>
-            <div class="form-item">
-              <label for="email" class="required">{{
-                $t("fields.email")
-              }}</label>
-              <input type="email" name="email" id="email" v-model="email" />
-              <small class="form-invalid-msg" v-if="errors['email']">{{
-                errors["email"][0]
-              }}</small>
-            </div>
+          <div class="form-item">
+            <label for="name" class="required">{{ $t("fields.name") }}</label>
+            <input type="text" name="name" id="name" v-model="name" />
+            <small class="form-invalid-msg" v-if="errors['name']">{{
+              errors["name"][0]
+            }}</small>
           </div>
           <div class="form-item">
             <label for="subject" class="required">{{
@@ -72,56 +61,18 @@ export default {
   data: () => ({
     errors: {},
     name: "",
-    email: "",
     subject: "",
     message: "",
   }),
   methods: {
     sendEmail() {
-      const data = {
-        name: this.name,
-        email: this.email,
-        subject: this.subject,
-        message: this.message,
-      };
-
-      fetch("/api/email", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }).then((res) => {
-        if (res.ok) {
-          this.$swal({
-            icon: "success",
-            title: this.$t("validations.email-send"),
-          });
-          this.name = null;
-          this.email = null;
-          this.subject = null;
-          this.message = null;
-        } else {
-          this.$swal({
-            icon: "error",
-            title: this.$t("validations.error"),
-            text: this.$t("validations.email-not-send"),
-          });
-        }
-      });
+      const mail = document.createElement("a");
+      mail.href = `mailto:juanpmachadob@gmail.com?subject=${this.subject} - ${this.name}&body=${encodeURIComponent(this.message)}`;
+      mail.click();
     },
     checkForm() {
-      this.$swal({
-        title: this.$t("validations.sending"),
-        text: this.$t("validations.please-wait"),
-        allowEscapeKey: false,
-        allowOutsideClick: false,
-      });
-      this.$swal.showLoading();
-
       this.errors = {
         name: [],
-        email: [],
         subject: [],
         message: [],
       };
@@ -136,86 +87,31 @@ export default {
         }
       };
 
-      let checkMaxLength = (fieldValue, length, fieldName) => {
-        if (fieldValue && fieldValue.length > length) {
-          this.errors[fieldName].push(
-            this.$tc("validations.max-length", {
-              field: this.$t(`fields.${fieldName}`),
-              n: length,
-            })
-          );
-        }
-      };
-
-      let checkMinLength = (fieldValue, length, fieldName) => {
-        if (fieldValue && fieldValue.length < length) {
-          this.errors[fieldName].push(
-            this.$tc("validations.min-length", {
-              field: this.$t(`fields.${fieldName}`),
-              n: length,
-            })
-          );
-        }
-      };
-
-      let checkEmailValidation = (fieldValue, fieldName) => {
-        const regex =
-          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!regex.test(fieldValue)) {
-          this.errors[fieldName].push(
-            this.$tc("validations.email", {
-              field: this.$t(`fields.${fieldName}`),
-            })
-          );
-        }
-      };
-
       let checkName = () => {
         let name = this.name;
         checkRequired(name, "name");
-        checkMinLength(name, 2, "name");
-        checkMaxLength(name, 45, "name");
-      };
-
-      let checkEmail = () => {
-        let email = this.email;
-        checkRequired(email, "email");
-        checkEmailValidation(email, "email");
-        checkMaxLength(email, 76, "email");
       };
 
       let checkSubject = () => {
         let subject = this.subject;
         checkRequired(subject, "subject");
-        checkMinLength(subject, 10, "subject");
-        checkMaxLength(subject, 100, "subject");
       };
 
       let checkMessage = () => {
         let message = this.message;
         checkRequired(message, "message");
-        checkMinLength(message, 20, "message");
-        checkMaxLength(message, 600, "message");
       };
 
       checkName();
-      checkEmail();
       checkSubject();
       checkMessage();
 
       if (
         !this.errors["name"][0] &&
-        !this.errors["email"][0] &&
         !this.errors["subject"][0] &&
         !this.errors["message"][0]
       ) {
         this.sendEmail();
-      } else {
-        this.$swal({
-          icon: "error",
-          title: this.$t("validations.error"),
-          text: this.$t("validations.please-validate"),
-        });
       }
     },
   },
--- client/src/main.js
+++ client/src/main.js
@@ -4,17 +4,10 @@ import router from "./router";
 
 import i18n from "./i18n";
 import VueEasyLightBox from 'vue-easy-lightbox'
-import VueSweetalert2 from "vue-sweetalert2";
-import "sweetalert2/dist/sweetalert2.min.css";
 
-const options = {
-  confirmButtonColor: '#008cff',
-};
-
-const app = createApp(App).use(i18n).use(i18n);
+const app = createApp(App);
 app.use(i18n);
 app.use(router);
 app.use(VueEasyLightBox)
-app.use(VueSweetalert2, options)
 
 app.mount("#app");
