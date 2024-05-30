b0c4371cd295cd6c8663bb2f41152df865076593
juanpmachadob <juanpmachadob@gmail.com>
2024-05-29 21:01:43 -0500

    feat: add Experience section to index page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from "./button/Button.astro";
+
+export { default as ExperienceCard } from "./experience-card/ExperienceCard.astro";
+export { default as ExperienceList } from "./experience-list/ExperienceList.astro";
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,2 +1,3 @@
+export { default as Experience } from "./Experience.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Navbar } from "./Navbar.astro";
