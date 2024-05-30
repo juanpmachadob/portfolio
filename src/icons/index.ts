f0fb6ba43718a2374932409cbe3a881dd904a3da
juanpmachadob <juanpmachadob@gmail.com>
2024-05-29 20:48:12 -0500

    feat: add Experience icon component

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,6 @@
 export { default as CurriculumIcon } from "./curriculum.astro";
 export { default as EmailIcon } from "./email.astro";
+export { default as ExperienceIcon } from "./experience.astro";
 export { default as GithubIcon } from "./github.astro";
 export { default as LinkedinIcon } from "./linkedin.astro";
 export { default as MenuIcon } from "./menu.astro";
