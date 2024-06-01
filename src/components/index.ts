3691214ddc7b55a0d7a1c36d63b7b6cc7e05f43d
juanpmachadob <juanpmachadob@gmail.com>
2024-06-01 14:24:06 -0500

    feat: add ProjectCard and ProjectsList components with responsive styles

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as Button } from "./button/Button.astro";
 
 export { default as ExperienceCard } from "./experience-card/ExperienceCard.astro";
 export { default as ExperienceList } from "./experience-list/ExperienceList.astro";
+
+export { default as ProjectCard } from "./project-card/ProjectCard.astro";
+export { default as ProjectsList } from "./projects-list/ProjectsList.astro";
