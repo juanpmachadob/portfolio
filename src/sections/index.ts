fa1b6e2b23dfe885c18eba648b1c400b2d2e7209
juanpmachadob <juanpmachadob@gmail.com>
2024-06-01 14:36:20 -0500

    feat: add Projects section to index page

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,3 +1,4 @@
 export { default as Experience } from "./Experience.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Navbar } from "./Navbar.astro";
+export { default as Projects } from "./Projects.astro";
