5ecbb607eaa0943add6b40480ad4ee9846f11ef6
juanpmachadob <juanpmachadob@gmail.com>
2024-06-02 16:41:09 -0500

    feat: add Footer to index page

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,5 +1,6 @@
 export { default as About } from "./About.astro";
 export { default as Experience } from "./Experience.astro";
+export { default as Footer } from "./Footer.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Navbar } from "./Navbar.astro";
 export { default as Projects } from "./Projects.astro";
