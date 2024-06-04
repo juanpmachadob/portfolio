ac18aeb3f1b0b7249021ba3db6e78682931ab316
juanpmachadob <juanpmachadob@gmail.com>
2024-06-03 22:35:02 -0500

    feat: add LanguageSelector component to Navbar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./button/Button.astro";
+export { default as LanguageSelector } from "./language-selector/LanguageSelector.astro";
 
 export { default as ExperienceCard } from "./experience-card/ExperienceCard.astro";
 export { default as ExperienceList } from "./experience-list/ExperienceList.astro";
