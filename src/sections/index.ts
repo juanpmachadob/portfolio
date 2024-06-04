cfe37d5e9a3a3a868e8f653e16a9e627ddbc465b
juanpmachadob <juanpmachadob@gmail.com>
2024-06-03 21:53:11 -0500

    refactor: move project details into separate component

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -3,4 +3,5 @@ export { default as Experience } from "./Experience.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Navbar } from "./Navbar.astro";
+export { default as ProjectDetails } from "./ProjectDetails.astro";
 export { default as Projects } from "./Projects.astro";
