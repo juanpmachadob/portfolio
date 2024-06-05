1e0984bb159fdacec71ab159f3b42bfaf6dc0fa2
juanpmachadob <juanpmachadob@gmail.com>
2024-06-04 21:56:08 -0500

    feat: add sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { DEFAULT_LOCALE, LOCALES } from "./src/i18n";
@@ -8,6 +9,7 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   site: "https://juanmachado.netlify.app",
   i18n: {
